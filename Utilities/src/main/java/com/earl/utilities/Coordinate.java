b7dba959fbb0b9db443ce4b93521c5c3b09d816c
Earl Harris <esharris@mac.com>
2022-10-18 16:32:01 -0400

    Added comment, replace StringBuffer with StringBuilder, fixed formatting.

--- Utilities/src/main/java/com/earl/utilities/Coordinate.java
+++ Utilities/src/main/java/com/earl/utilities/Coordinate.java
@@ -3,68 +3,76 @@ package com.earl.utilities;
 import java.util.Objects;
 
 /**
- * 
+ *
  * @author earlharris
  *
  */
 public final class Coordinate {
 
-	public static final Coordinate ORIGIN = new Coordinate(0, 0);
+    public static final Coordinate ORIGIN = new Coordinate(0, 0);
 
-	private final int x;
-	private final int y;
+    private final int x;
+    private final int y;
 
-	public Coordinate(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    /**
+     *
+     * @param x
+     * @param y
+     */
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public Coordinate add(Coordinate coordinate) {
-		return new Coordinate(x + coordinate.getX(), y + coordinate.getY());
-	}
+    public Coordinate add(Coordinate coordinate) {
+        return new Coordinate(x + coordinate.getX(), y + coordinate.getY());
+    }
 
-	public Coordinate subtract(Coordinate coordinate) {
-		return new Coordinate(x - coordinate.getX(), y - coordinate.getY());
-	}
+    public Coordinate subtract(Coordinate coordinate) {
+        return new Coordinate(x - coordinate.getX(), y - coordinate.getY());
+    }
 
-	public Coordinate mult(int i) {
-		return new Coordinate(i * this.x, i * this.y);
-	}
+    public Coordinate mult(int i) {
+        return new Coordinate(i * this.x, i * this.y);
+    }
 
-	public int getX() {
-		return x;
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getY() {
-		return y;
-	}
+    public int getY() {
+        return y;
+    }
 
-	@Override
-	public String toString() {
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append(Coordinate.class);
-		stringBuffer.append("(");
-		stringBuffer.append(x);
-		stringBuffer.append(", ");
-		stringBuffer.append(y);
-		stringBuffer.append(")");
-		return stringBuffer.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder stringBuffer = new StringBuilder();
+        stringBuffer.append(Coordinate.class);
+        stringBuffer.append("(");
+        stringBuffer.append(x);
+        stringBuffer.append(", ");
+        stringBuffer.append(y);
+        stringBuffer.append(")");
+        return stringBuffer.toString();
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(x, y);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Coordinate other = (Coordinate) obj;
-		return x == other.x && y == other.y;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Coordinate other = (Coordinate) obj;
+        return x == other.x && y == other.y;
+    }
 }
